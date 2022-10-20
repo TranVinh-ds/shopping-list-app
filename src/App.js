cc83f3f43fec70a4b5a8f6ac1899de12a15de58a
TranVinh-ds <chenvinh2003@gmail.com>
2022-10-20 15:51:18 +0700

    create CRUD operations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchItem from './components/SearchItem';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import { useState, useEffect } from 'react';
+import apiRequest from './components/apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -32,19 +33,7 @@ function App() {
     }, 2000);
   }, []);
 
-  const handleClickCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-  };
-
-  const handleClickDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const itemObj = {
       id: items.length ? items[items.length - 1].id + 1 : 1,
@@ -52,8 +41,50 @@ function App() {
       item: newItem,
     };
     setItems(items.concat(itemObj));
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(itemObj),
+    };
+
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
     setNewItem('');
   };
+
+  const handleClickCheck = async (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+
+    const myItems = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({ checked: myItems[0].checked }),
+    };
+
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
+  };
+
+  const handleClickDelete = async (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
+  };
+
   return (
     <div className="App">
       <Header title="Shopping List App" />
