128f26ea716d3e7408470be85c633c0235748b73
TranVinh-ds <chenvinh2003@gmail.com>
2022-10-20 11:52:48 +0700

    setup blueprint for shopping list app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Shopping List App" />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <main>
+      <h2>shopping list</h2>
+    </main>
+  );
+};
+
+export default Content;
